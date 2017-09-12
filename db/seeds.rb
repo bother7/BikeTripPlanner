e17eeea1fc16803e15e36629f4dfd6813cae1d96
Zali Perlow <zali.perlow@flatironschool.com>
2017-09-12 17:33:11 -0400

    added seed command to populate stations table

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,11 @@ Trip.create([
   {origin: "zalis house", destination: "flatiron"},
   {origin: "joes house", destination: "america"},
   ])
+
+  require 'csv'
+
+  csv_text = File.read('app/models/stops.csv')
+  csv = CSV.parse(csv_text, :headers => true)
+  csv.each do |row|
+    Station.create!(row.to_hash)
+  end
