40618e6252adb27cd4e5b1debd4e3599f13d30e3
Zali Perlow <zali.perlow@flatironschool.com>
2017-09-15 13:19:15 -0400

    add users and basic login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913143753) do
+ActiveRecord::Schema.define(version: 20170914190315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20170913143753) do
   end
 
   create_table "updatetrains", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.integer "station_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
